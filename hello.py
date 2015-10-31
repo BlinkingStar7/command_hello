888c067ac5b8d06397ad104b578019446893074f
BlinkingStar7 <BlinkingStar7@users.noreply.github.com>
2015-10-31 15:57:55 +0900

    GitHub for gitpull

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("Hello world!")
 print("Tell Your World")
 print("Tell His World")
 print("Tell Her World")
+print("Tell My World")
 // For command line git tutorial
 // Local repository modification
 // GutHub modification
